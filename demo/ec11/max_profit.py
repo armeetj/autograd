ab8aeb1c620d8a6908592600b8d8c2f7822d0a20
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2024-01-09 16:41:22 -0800

    example problem maximizing profit for a business

---
+++ demo/ec11/max_profit.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append("../../")
+from autograd.engine import Value
+import random
+
+verbose = True
+lr = 0.001
+epochs = 20000
+x = Value(random.randint(0, 200))
+y = Value(random.randint(0, 200))
+
+
+for i in range(epochs + 1):
+    # forward
+    q = x + y
+    profit = (q * (200 - q)) - (100 * x) - (y ** 2)
+    profit = -profit # we want to maximize profit so negate the entire expression
+
+    # backward
+    x.zero_grad()
+    y.zero_grad()
+    profit.backward()
+    
+    # update params
+    x.data -= lr * x.grad
+    y.data -= lr * y.grad
+
+    # debugging info
+    if verbose and i % (epochs / 10) == 0:        
+        print(f"[{i}]\tx, y: {x.data} {y.data}: profit(x, y): {-profit.data}")
